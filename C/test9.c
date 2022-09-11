79ad0890b720fbcb7c06ac19b8607ac3bb34b6f6
aklk1ng <2628084745@qqq.com>
2022-09-11 20:40:36 +0800

    update

--- C/test9.c
+++ C/test9.c
@@ -5,22 +5,18 @@ int main()
 {
     int i = 0;
     int count = 0;
-    for (i = 101; i <= 200; i+=2)
-    {
+    for (i = 101; i <= 200; i+=2) {
         //判断素数  只能被1和它本身整除
         //2->i-1之间的数字去试除i
         int j = 0;
         int flag = 1;
-        for (j = 2; j <= sqrt(i); j++)//sqrt - 开平方函数 - 头文件为math.h
-        {
-            if (i % j == 0)
-            {
+        for (j = 2; j <= sqrt(i); j++) {//sqrt - 开平方函数 - 头文件为math.h
+            if (i % j == 0) {
                 flag = 0;//不是素数
                 break;
             }
         }
-        if (flag == 1)
-        {
+        if (flag == 1) {
             count++;
             printf("%d ", i);//素数
         }
