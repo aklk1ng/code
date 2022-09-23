df3780fd98edcf4d75fdf5c6d1f0a8e4f3591828
aklk1ng <2628084745@qqq.com>
2022-09-23 21:50:53 +0800

    update

---
+++ C/SeqList/tree.c
@@ -0,0 +1,136 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+typedef int DataType;
+typedef struct TreeNode
+{
+    DataType data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+}TNode;
+TNode *TreeInsert(TNode *root, DataType data)
+{
+    if(root == NULL) {
+        TNode *node = (TNode*)malloc(sizeof(TNode));
+        if(node == NULL) {
+            printf("malloc falied\n");
+            return NULL;
+        }
+        node->data=data;
+        node->left=NULL;
+        node->right=NULL;
+        return node;
+    }
+    else if(root->data > data) {
+        root->left = TreeInsert(root->left, data);
+    }
+    else {
+        root->right = TreeInsert(root->right, data);
+    }
+    return root;
+
+}
+//递归
+TNode *TreeFind(TNode *root, DataType data)
+{
+    if(root ==NULL) {
+        printf("the tree is empty!\n");
+        return NULL;
+    }
+    if(root->data > data) {
+        TreeFind(root->left , data);
+    }
+    else if(root->data < data) {
+        TreeFind(root->right, data);
+    }
+    else
+    return root;
+}
+TNode *Tree_Max(TNode *root)
+{
+    if(root ==NULL) {
+        printf("the tree is empty!]n");
+        return NULL;
+    }
+    else if(root->right) {
+        return root;
+    }
+    else {
+        return Tree_Max(root->right);
+    }
+}
+TNode *Tree_Min(TNode *root)
+{
+    if(root ==NULL) {
+        printf("the tree is empty!]n");
+        return NULL;
+    }
+    else if(root->left) {
+        return root->left;
+    }
+    else {
+        return Tree_Min(root->left);
+    }
+}
+TNode *TreeDelate(TNode *root, DataType data)
+{
+    if(root == NULL) {
+        printf("the tree is empty!\n");
+        return root;
+    }
+    TNode *tmp;
+    if(root->data > data) {
+        root->left = TreeDelate(root->left, data);
+    }
+    else if(root->data < data) {
+        root->right = TreeDelate(root->right, data);
+    }
+    else {
+        if(root->left && root->right) {
+            tmp = Tree_Min(root->right);
+            root->data = tmp->data;
+            root->right = TreeDelate(root, data);
+        }
+        else {
+            tmp = root;
+            if(root->left == NULL) {
+                root = root->right;
+            }
+            else {
+                root = root->left;
+            }
+        }
+    }
+    return root;
+}
+//先序遍历
+void PreOrderTree(TNode *root)
+{
+    if(root == NULL) {
+        printf("the tree is empty!\n");
+        return root;
+    }
+    else {
+        printf("d ", root->data);
+        PreOrderTree(root->left);
+        PreOrderTree(root->right);
+    }
+}
+//中序遍历
+void InOrderTree(TNode *root)
+{
+    if(root == NULL) {
+        printf("the tree is empty!\n");
+        return root;
+    }
+    else {
+        PreOrderTree(root->left);
+        printf("d ", root->data);
+        PreOrderTree(root->right);
+    }
+}
+int main(int argc, char const *argv[])
+{
+    TNode *root =NULL;
+    return 0;
+}
