a021cc0c0bdb155eda02061cd74bf9e07905c713
aklk1ng <2628084745@qq.com>
2022-10-30 13:09:29 +0800

    array template

---
+++ c++/template/main.cpp
@@ -0,0 +1,10 @@
+#include"myArray.h"
+void test()
+{
+    MyArray <int>arr1(5);
+}
+int main (int argc, char *argv[])
+{
+    test();
+    return 0;
+}
--- c++/template/myArray.h
+++ c++/template/myArray.h
@@ -5,17 +5,50 @@ template<class T>
 class MyArray
 {
     public:
+        //有参构造
         MyArray(int capacity) {
+            cout << "有参构造"<<endl;
             this->m_Capacity = capacity;
             this->m_Size = 0;
             this->pAddress = new T[this->m_Capacity];
         }
+        MyArray(const MyArray& arr) {
+            cout << "拷贝构造"<<endl;
+            this->m_Capacity = arr.m_Capacity;
+            this->m_Size = arr.m_Size;
+
+            this->pAddress = new T[arr.m_Capacity];
+            for (int i = 0; i < this->m_Size; i++) {
+                this->pAddress[i] = arr.pAddress[i];
+            }
+        }
+        //operator
+        MyArray& operator=(const MyArray& arr) {
+            cout << "operator构造"<<endl;
+            if (this->pAddress != NULL) {
+                delete[] this->pAddress;
+                this->pAddress = NULL;
+                this->m_Capacity = 0;
+                this->m_Size = 0;
+            }
+            //深拷贝
+            this->m_Capacity = arr.m_Capacity;
+            this->m_Size = arr.m_Size;
+            this->pAddress = new T[this->m_Capacity];
+            for (int i = 0; i < this->m_Size; i++) {
+                this->pAddress[i] = arr.pAddress[i];
+            }
+            return *this;
+        }
         ~MyArray() {
+            cout << "析构"<<endl;
             if (this->pAddress != NULL) {
                 delete [] this->pAddress;
                 this->pAddress = NULL;
             }
         }
     private:
-        int m_Capacity;
+        T m_Capacity;
+        T* pAddress;
+        T m_Size;
 };
