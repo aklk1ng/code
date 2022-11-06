88404b0cd7fc7f956d599c6d16401117d3c6e627
aklk1ng <2628084745@qq.com>
2022-11-06 18:33:23 +0800

    update socket

--- coding-for-c/C/socket/client.c
+++ coding-for-c/C/socket/client.c
@@ -8,17 +8,17 @@
 int main (int argc, char *argv[])
 {
     //1.创建通信的套接字
-    int fd= socket(AF_INET, SOCK_STREAM, 0);
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd == -1) {
         perror("socket");
         return -1;
     }
     //2.连接服务器IP port
     struct sockaddr_in saddr;
-    saddr.sin_family= AF_INET;
-    saddr.sin_port= htons(9999);
+    saddr.sin_family = AF_INET;
+    saddr.sin_port = htons(9999);
     inet_pton(AF_INET, "192.168.251.82", &saddr.sin_addr.s_addr);
-    int ret= connect(fd, (struct sockaddr*)&saddr, sizeof(saddr));
+    int ret = connect(fd, (struct sockaddr*)&saddr, sizeof(saddr));
     if (ret == -1) {
         perror("connect");
         return -1;
@@ -34,11 +34,11 @@ int main (int argc, char *argv[])
 
         //接收数据
         memset(buff, 0, sizeof(buff));
-        int len= recv(fd, buff, sizeof(buff), 0);
+        int len = recv(fd, buff, sizeof(buff), 0);
         if (len > 0) {
             printf("server say: %s\n", buff);
         }
-        else if (len ==0) {
+        else if (len == 0) {
             printf("the server disconnect...\n");
             break;
         }
