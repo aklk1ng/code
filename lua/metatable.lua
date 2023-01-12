7e5f8aa433e10f4d86a6441dfc046a3e4b71642c
aklk1ng <2628084745@qq.com>
2023-01-12 21:21:58 +0800

    update the lua code

---
+++ lua/metatable.lua
@@ -0,0 +1,23 @@
+f1 = {a = 1, b = 2}
+for key, value in pairs(f1) do
+    print(key, value)
+end
+f2 = {a = 2, b = 3}
+local metafraction = {}
+function metafraction.__add(f1, f2)
+    local sum = {}
+    sum.b = f1.b * f2.b
+    sum.a = f1.a * f2.a
+    return sum
+end
+setmetatable(f1, metafraction)
+setmetatable(f2, metafraction)
+s = f1 + f2
+-- print(s['a'], s['b'])
+print(s.a, s.b)
+
+local defaultFavs = {animal = 'gru', food = 'donuts'}
+local myFavs = {food = 'pizza'}
+setmetatable(myFavs, {__index = defaultFavs})
+local print_eatenBy = myFavs.animal
+print(print_eatenBy)
