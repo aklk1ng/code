bb1c4b559715a9796d713bb36aa619dfd496ea1d
aklk1ng <2628084745@qq.com>
2023-02-05 20:53:48 +0800

    upate lua

--- lua/string.lua
+++ lua/string.lua
@@ -2,3 +2,9 @@ print(string.byte("abc"))
 print(string.char(97, 98, 99, 100))
 print(string.len("fasdf"))
 print(string.rep("fasdf", 2))
+
+local str = "asdfds134"
+if string.find(str, "asd") then
+    local new_str = string.gsub(str, "asd", "aassdd", 1)
+    print(new_str)
+end
