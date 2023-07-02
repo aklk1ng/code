ef2a1f738b7901b8907a33edbd8e49905736d193
aklk1ng <2628084745@qq.com>
2023-07-02 18:54:35 +0800

    update leetcode

---
+++ cpp/leetcode/1351.统计有序矩阵中的负数.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Soluton {
+public:
+  int countNegatives(vector<vector<int>> &grid) {
+    int n = grid.size();
+    int m = grid[0].size();
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (grid[i][j] < 0)
+          cnt++;
+      }
+    }
+    return cnt;
+  }
+};
---
+++ cpp/leetcode/628.三个数的最大乘积.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Soluton {
+public:
+  int maximumProduct(vector<int> &nums) {
+    int n = nums.size();
+    if (n < 3)
+      return INT_MIN;
+    return max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
+  }
+};
