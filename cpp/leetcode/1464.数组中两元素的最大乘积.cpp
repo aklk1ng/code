87a0ab135a0863c484aee946c88a54bbffea329a
aklk1ng <2628084745@qq.com>
2023-07-08 17:14:52 +0800

    update leetcode

---
+++ cpp/leetcode/1464.数组中两元素的最大乘积.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Soluton {
+public:
+  int maxProduct(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    return (nums[nums.size() - 1] - 1) * (nums[nums.size() - 2] - 1);
+  }
+};
---
+++ cpp/leetcode/2255.统计是给定字符串前缀的字符串数目.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Soluton {
+public:
+  int countPrefixes(vector<string> &words, string s) {
+    int res = 0;
+    auto isPrefix = [&](const string &word) -> bool {
+      if (s.size() < word.size())
+        return false;
+
+      for (int i = 0; i < word.size(); i++)
+        if (word[i] != s[i])
+          return false;
+      return true;
+    };
+
+    for (auto word : words)
+      if (isPrefix(word))
+        res++;
+    return res;
+  }
+};
