cff946d865cb14814dc7d01409834f417ebae769
aklk1ng <2628084745@qq.com>
2023-07-20 17:48:43 +0800

    update leetcode

---
+++ cpp/leetcode/350.两个数组的交集II.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Soluton1 {
+public:
+  vector<int> intersect(vector<int> &nums1, vector<int> &nums2) {
+    unordered_map<int, int> m;
+    for (auto num : nums1)
+      m[num]++;
+
+    vector<int> res;
+    for (auto num : nums2) {
+      if (m.count(num)) {
+        res.push_back(num);
+        m[num]--;
+      }
+      if (m[num] == 0)
+        m.erase(num);
+    }
+    return res;
+  }
+};
+
+class Soluton2 {
+public:
+  vector<int> intersect(vector<int> &nums1, vector<int> &nums2) {
+    int len1 = nums1.size(), len2 = nums2.size();
+    int ptr1 = 0, ptr2 = 0;
+    vector<int> res;
+    while (ptr1 < len1 && ptr2 < len2) {
+      if (nums1[ptr1] < nums2[ptr2])
+        ptr1++;
+      else if (nums1[ptr1] > nums2[ptr2])
+        ptr2++;
+      else {
+        res.push_back(nums1[ptr1]);
+        ptr1++;
+        ptr2++;
+      }
+    }
+    return res;
+  }
+};
