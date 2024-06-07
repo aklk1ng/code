7f02803d8f948148650ac2abfc3c43de3bfedcd3
aklk1ng <2628084745@qq.com>
2024-06-07 18:36:23 +0800

    update

--- cpp/cf/contest/1102/f.cpp
+++ cpp/cf/contest/1102/f.cpp
@@ -1,67 +1,67 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define ll long long
 const int INF = 1e9;
 
 int n, m;
-vector<vector<int>> mn1, mn2;
+std::vector<std::vector<int>> mn1, mn2;
 int dp[1 << 18][18];
 
 int cal(int mask, int v) {
-  if (dp[mask][v] != -1) {
-    return dp[mask][v];
-  }
-  dp[mask][v] = 0;
-  for (int u = 0; u < n; u++) {
-    if (u != v && ((mask >> u) & 1)) {
-      dp[mask][v] = max(dp[mask][v], min(mn1[u][v], cal(mask ^ (1 << v), u)));
+    if (dp[mask][v] != -1) {
+        return dp[mask][v];
     }
-  }
-  return dp[mask][v];
+    dp[mask][v] = 0;
+    for (int u = 0; u < n; u++) {
+        if (u != v && ((mask >> u) & 1)) {
+            dp[mask][v] = std::max(
+                dp[mask][v], std::min(mn1[u][v], cal(mask ^ (1 << v), u)));
+        }
+    }
+    return dp[mask][v];
 }
 
 void solve() {
-  cin >> n >> m;
-  vector a(n, vector<int>(m));
-  mn1.assign(n, vector<int>(n, 0));
-  mn2.assign(n, vector<int>(n, 0));
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
-      cin >> a[i][j];
+    std::cin >> n >> m;
+    std::vector a(n, std::vector<int>(m));
+    mn1.assign(n, std::vector<int>(n, 0));
+    mn2.assign(n, std::vector<int>(n, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            std::cin >> a[i][j];
+        }
     }
-  }
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      int val = INF;
-      for (int k = 0; k < m; k++) {
-        val = min(val, abs(a[i][k] - a[j][k]));
-      }
-      mn1[i][j] = val;
-      val = INF;
-      for (int k = 0; k < m - 1; k++) {
-        val = min(val, abs(a[i][k] - a[j][k + 1]));
-      }
-      mn2[i][j] = val;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int val = INF;
+            for (int k = 0; k < m; k++) {
+                val = std::min(val, abs(a[i][k] - a[j][k]));
+            }
+            mn1[i][j] = val;
+            val = INF;
+            for (int k = 0; k < m - 1; k++) {
+                val = std::min(val, abs(a[i][k] - a[j][k + 1]));
+            }
+            mn2[i][j] = val;
+        }
     }
-  }
 
-  int ans = 0;
-  for (int i = 0; i < n; i++) {
-    memset(dp, -1, sizeof(dp));
-    for (int j = 0; j < n; j++) {
-      dp[1 << j][j] = (j == i ? INF : 0);
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        memset(dp, -1, sizeof(dp));
+        for (int j = 0; j < n; j++) {
+            dp[1 << j][j] = (j == i ? INF : 0);
+        }
+        for (int j = 0; j < n; j++) {
+            ans = std::max(ans, std::min(mn2[j][i], cal((1 << n) - 1, j)));
+        }
     }
-    for (int j = 0; j < n; j++) {
-      ans = max(ans, min(mn2[j][i], cal((1 << n) - 1, j)));
-    }
-  }
-  cout << ans << '\n';
+    std::cout << ans << '\n';
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  solve();
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve();
 
-  return 0;
+    return 0;
 }
